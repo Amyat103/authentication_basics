e1e913a9ce3ed3c423747589b79ee0b2ea5638d9
Amyat103 <zhoudavid7312@gmail.com>
2024-06-22 15:59:10 -0400

    password authentication

--- app.js
+++ app.js
@@ -21,3 +21,47 @@ const User = mongoose.model(
     password: { type: String, required: true },
   })
 );
+
+const app = express();
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
+
+app.get('/sign-up', (req, res) => res.render('sign-up-form'));
+
+app.post('/sign-up', async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    const result = await user.save();
+    res.redirect('/');
+  } catch (err) {
+    return next(err);
+  }
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
